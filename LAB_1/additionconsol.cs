1e9fc0d3c9c2d61861e8c8fce85caaa36d37a1fc
yash garala <yashgarala29@gmail.com>
2020-07-31 19:42:38 +0530

    Create additionconsol.cs

---
+++ LAB_1/additionconsol.cs
@@ -0,0 +1,15 @@
+using System;
+namespace drive_addtion
+{
+	public class driver_addition
+	{
+		static void Main(string[] arg)
+		{
+			string n1= Console.ReadLine();
+			string n2 = Console.ReadLine();
+			
+			Console.WriteLine(Int32.Parse(n1)+Int32.Parse(n2));
+			Console.ReadLine();
+		}
+	}
+}
