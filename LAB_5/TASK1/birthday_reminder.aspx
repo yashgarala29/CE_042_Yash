39874c2dc1e1136f3cf2db820d20ba3577079c69
yash garala <yashgarala29@gmail.com>
2020-08-06 17:49:55 +0530

    TASK 1
    BIRTH DAY REMINDER PROGRAME

---
+++ LAB_5/TASK1/birthday_reminder.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="birthday_reminder.aspx.cs" Inherits="TASK1.birthday_reminder" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <asp:TextBox ID="Name" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Birth Date<br />
+            <br />
+            <asp:TextBox ID="birthday" runat="server" TextMode="Date"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="remember" runat="server" OnClick="remember_Click" Text="remember" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="delete" runat="server" OnClick="delete_Click" style="margin-left: 8px" Text="delete" Width="152px" />
+            <br />
+        </div>
+    &nbsp;name=<asp:Label ID="name_l" runat="server"></asp:Label>
+&nbsp;&nbsp;
+        <br />
+        birth date=<asp:Label ID="date" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ LAB_5/TASK1/birthday_reminder.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TASK1
+{
+    public partial class birthday_reminder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie httpCookie1 = Request.Cookies["date"];
+            if (httpCookie1!=null)
+            {
+                name_l.Text = httpCookie1["name_c"];
+                date.Text = httpCookie1["date_c"];
+                httpCookie1.Expires = DateTime.Now.AddMinutes(2);
+                Response.Cookies.Add(httpCookie1);
+
+            }
+            else
+            {
+                name_l.Text = null;
+                date.Text = null;
+
+            }
+        }
+
+        protected void remember_Click(object sender, EventArgs e)
+        {
+            
+           
+                HttpCookie httpCookie = new HttpCookie("date");
+                httpCookie["name_c"] = Name.Text; 
+                httpCookie["date_c"] = birthday.Text;
+                 name_l.Text = Name.Text;
+                date.Text = birthday.Text;
+            Response.Cookies.Add(httpCookie);
+            
+               
+            
+         }
+
+        protected void delete_Click(object sender, EventArgs e)
+        {
+            HttpCookie httpCookie = new HttpCookie("date");
+            httpCookie.Expires =DateTime.Now.AddMinutes(0);
+      
+                Response.Cookies.Add(httpCookie);
+            
+            
+        }
+    }
+}
