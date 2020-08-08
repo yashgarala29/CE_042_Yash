d0a8e67afc4729403a7451c4f635c0e4bba33ba7
yash garala <yashgarala29@gmail.com>
2020-08-08 19:23:13 +0530

    lab 3

---
+++ LAB_3/calc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calc
+{ 
+public class calc
+{
+	public long add(int n1,int n2)
+	{
+			return n1 + n2;
+	}
+	public long mul(int n1,int n2)
+        {
+			return n1 * n2;
+        }
+}
+
+}
---
+++ LAB_3/new_calc.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+[assembly: AssemblyVersion("1.0.0.0")]
+namespace calc
+{
+	public class calc
+	{
+		public long add(int n1, int n2)
+		{
+			return n1 + n2;
+		}
+		public long mul(int n1, int n2)
+		{
+			return n1 * n2;
+		}
+	}
+
+}
